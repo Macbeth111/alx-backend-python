cb18ac6183166e50f392c244fd4201b6c1ddb4af
Macbeth Obimpeh <macbeth.obimpeh@gmail.com>
2025-08-02 18:46:39 +0000

    Task 3: Add threaded conversations with parent_message field

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -17,6 +17,13 @@ class Message(models.Model):
         on_delete=models.SET_NULL,
         related_name='edited_messages'
     )
+    parent_message = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='replies'
+    )
 
 
 class MessageHistory(models.Model):
